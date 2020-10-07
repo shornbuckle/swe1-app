ab47c8f064de676b799f769a9f12ac3a81abb60b
sh4986 <65291545+shornbuckle@users.noreply.github.com>
2020-10-07 01:02:44 -0400

    updated migration

--- polls/migrations/0001_initial.py
+++ polls/migrations/0001_initial.py
@@ -3,6 +3,8 @@
 from django.db import migrations, models
 import django.db.models.deletion
 
+from django.db import migrations, models
+import django.db.models.deletion
 
 class Migration(migrations.Migration):
 
